82f258b83013b7fec8a488167158529140f2ef86
Gilcemir <gangelofilho@gmail.com>
2024-03-29 17:55:26 -0300

    initialCommit

---
+++ backend/src/ServerlessAPI/Entities/Book.cs
@@ -0,0 +1,10 @@
+namespace ServerlessAPI.Entities;
+
+public class Book
+{
+    public Guid Id { get; set; } = Guid.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string? ISBN { get; set; }
+    public List<string>? Authors { get; set; }
+    public string? CoverPage { get; set; }
+}
